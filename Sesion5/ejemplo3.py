cca6c1f1ea07983e11fb3be5b74106f2371b82a7
robert24 <robertodavid2405@gmail.com>
2024-02-17 09:07:47 -0600

    Ejercicio Listas del profe

---
+++ Sesion5/ejemplo3.py
@@ -0,0 +1,13 @@
+#listas
+lista = [1, 2, 3, "Manzana", 15.6, True]
+print(lista)
+
+#imprimir el primer elemento de la lista
+print(lista[0])
+
+#tamano de la lista
+tam = len(lista)
+print(f"El tamano es {tam}")
+
+#imprimir el ultimo valor
+print(lista[tam-1])
