7b885751b399c0c53d0cf83596ac38b4a2eff183
robert24 <robertodavid2405@gmail.com>
2024-02-17 09:16:04 -0600

    Se modifico el ejercicio Listas Ejemplo3

--- Sesion5/ejemplo3.py
+++ Sesion5/ejemplo3.py
@@ -11,3 +11,10 @@ print(f"El tamano es {tam}")
 
 #imprimir el ultimo valor
 print(lista[tam-1])
+
+#imprimir los elementos 4 hasta 6
+print(lista[3:7])
+
+#agregar un elemento a la lista
+lista.append("Roberto")
+print(lista)
